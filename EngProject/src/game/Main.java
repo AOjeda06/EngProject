b3de37f6722fc5303859d888fd590633874f55f1
gs.villanueva <gs.villanueva@106-23.info2>
2025-03-03 11:54:26 +0100

    Avance

--- EngProject/src/game/Main.java
+++ EngProject/src/game/Main.java
@@ -2,9 +2,25 @@ package game;
 
 public class Main {
 
-	public static void main(String[] args) {
+	static char tablero[][] = new char[10][10];
+	static int n;
+	static int m;
 
-		int flyes;
+	public static void main(String[] args) {
+		inicializarTablero();
 	}
 
+	private static void inicializarTablero() {
+		int numColumnas = 1;
+		for (int i = 0 ; i < n ; i++) {
+			System.out.print((int)(numColumnas+i) + "\t");
+		}
+		
+		for (int i = 0 ; i < n ; i++) {
+			for (int j  = 0 ; i < m ; i++) {
+				System.out.print(" ");
+			}
+		}
+		
+	}
 }
